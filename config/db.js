3a15c3152dbd462f39a8abefb45f198e53d671a7
quanghny <nguyenquanghuy208@gmail.com>
2021-12-24 15:29:17 +0700

    v1'

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
-const uri = `mongodb+srv://${process.env.USERNAME_MONGODB}:${process.env.PASSWORD_MONGODB}@doantn.qrqas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.USERNAME_MONGODB}:${process.env.PASSWORD_MONGODB}@cluster0.vl3oh.mongodb.net/solar-tracking?retryWrites=true&w=majority`;
 async function connect() {
-	try {
-		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log('Connecting successfully');
-	} catch (error) {
-		console.log('Connecting failure');
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connecting successfully');
+  } catch (error) {
+    console.log('Connecting failure');
+    process.exit(1);
+  }
 }
 
 module.exports = {
-	connect,
+  connect,
 };
